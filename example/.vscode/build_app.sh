34c149c090cfd1bf472368309aeec1109778fe70
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-01-05 14:38:22 +0200

    updated build

--- example/.vscode/build_app.sh
+++ example/.vscode/build_app.sh
@@ -8,7 +8,5 @@ DEVICE_NAME="$6"
 
 DESTINATION="platform=$PLATFORM,OS=$OS,name=$DEVICE_NAME"
 
-#xcodebuild -workspace $PROJECT_FILE -scheme $SCHEME -configuration Debug -destination "$DESTINATION" -sdk iphonesimulator build
-xcodebuild -workspace $PROJECT_FILE -scheme $SCHEME -configuration Debug -destination "$DESTINATION" -sdk iphonesimulator build | tee .vscode/build.log; xcode-build-server parse build.log >/dev/null 2>&1
-
-#xcode-build-server postaction | bash &
+xcodebuild -workspace $PROJECT_FILE -scheme $SCHEME -configuration Debug -destination "$DESTINATION" -sdk iphonesimulator build
+#xcodebuild -workspace $PROJECT_FILE -scheme $SCHEME -configuration Debug -destination "$DESTINATION" -sdk iphonesimulator build | tee .vscode/build.log; xcode-build-server parse build.log >/dev/null 2>&1
