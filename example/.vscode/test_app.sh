b5cb27ffe131f9641bc185c6f9a36a2c558282ac
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-01-07 14:47:30 +0200

    enhanced testing script

--- example/.vscode/test_app.sh
+++ example/.vscode/test_app.sh
@@ -1,6 +1,7 @@
 
 source '.vscode/.env'
 DESTINATION="platform=$PLATFORM,OS=$PLATFORM_OS,name=$DEVICE_NAME"
-xcodebuild test -workspace $PROJECT_FILE -scheme $PROJECT_SCHEME -configuration Debug -sdk iphonesimulator -destination "$DESTINATION" 
+xcodebuild test -workspace $PROJECT_FILE -scheme $PROJECT_SCHEME -configuration Debug -sdk iphonesimulator -destination "$DESTINATION" | tee '.vscode/tests.log'
 
+echo 'Your testing results are in: .vscode/tests.log'
 echo 'To open xcresult in Xcode perform: open -a XCode /path_to_your_testing.xcresult'
