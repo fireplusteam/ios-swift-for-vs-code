43b9a10eac40a2c16cc98dd91bc1566778e7fac7
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-01-29 14:05:13 +0200

    Fixed compile file script

--- example/.vscode/compile_current_file.sh
+++ example/.vscode/compile_current_file.sh
@@ -19,8 +19,6 @@ import helper
 import xcutil
 
 scheme = xcutil.get_scheme_by_file_name("$PROJECT_FILE", "$SELECTED_FILE")
-if scheme is None and ".swift" in "$SELECTED_FILE":
-    scheme = "$PROJECT_SCHEME"
 print(scheme)
 
 EOF
@@ -30,17 +28,26 @@ SCHEME=$(echo "$SCHEME_SCRIPT" | tail -n 1)
 
 SCHEME_VALUE=$(echo "$SCHEME")
 
+export continueBuildingAfterErrors=True
+
+
 if [ "$SCHEME_VALUE" == "None" ]; then
-    echo "No scheme is found for file: $SELECTED_FILE"
-    exit 0
+    if [[ "$SELECTED_FILE" == *.swift ]]; then
+        echo "File is not found in target, main target is building instead"
+        SCHEME_VALUE=$PROJECT_FILE
+    else
+        echo "No scheme is found for file: $SELECTED_FILE"
+        exit 0
+    fi
+else
+    # file is in project, update single file
+    export BUILD_XCODE_SINGLE_FILE_PATH="$SELECTED_FILE"
 fi
 
 rm .logs/build.log
 
-echo "UPDATING INDEXING FOR: ${SCHEME_VALUE}"
+echo "UPDATING INDEXING FOR: ${SCHEME_VALUE}, file: $SELECTED_FILE"
 
-export continueBuildingAfterErrors=True
-export BUILD_XCODE_SINGLE_FILE_PATH="$SELECTED_FILE"
 
 xcodebuild $TYPE $PROJECT_FILE -scheme $SCHEME -configuration Debug -destination "$DESTINATION" -sdk iphonesimulator -resultBundlePath "$BUNDLE" build 2> /dev/null | tee -a '.logs/build.log' &> /dev/null 2>&1
 
