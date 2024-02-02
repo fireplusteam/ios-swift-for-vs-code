67220a5d8d55b185dbfb312e251e12caf228c9b6
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-02-02 18:33:42 +0200

    Fixed termination script

--- example/.vscode/terminate_current_running_app.sh
+++ example/.vscode/terminate_current_running_app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source '.vscode/.env'
 
+python3 .vscode/terminate_current_running_app.py
+
 # Stop previously running app
 xcrun simctl terminate "$DEVICE_ID" "$BUNDLE_APP_NAME"
 
@@ -8,5 +10,3 @@ if [ $? -eq 1 ]; then
     echo "Termination failed."
     exit 0
 fi
-
-python3 .vscode/terminate_current_running_app.py
