e700587fb77a038fb97b12aec4322216a0e1c81c
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2024-10-14 20:40:39 +0300

    bumped version to 0.4.0

--- make.sh
+++ make.sh
@@ -15,4 +15,4 @@ pyinstaller --onefile src/XCBBuildServiceProxy/XCBBuildService.py
 npm run compile
 vsce package
 
-code --install-extension vscode-ios-0.3.3.vsix
+code --install-extension vscode-ios-0.4.0.vsix
