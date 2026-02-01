a25f28597961a320a92e15347bed06ad57aeef1d
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2026-02-01 21:09:04 +0200

    bumped to 0.6.12

--- make.sh
+++ make.sh
@@ -19,7 +19,7 @@ npm run test
 # locally package and install the extension
 vsce package --target darwin-arm64
 vsce package
-code --install-extension vscode-ios-0.6.11.vsix
+code --install-extension vscode-ios-0.6.12.vsix
 
 # to publish the extension, uncomment the following line
 # https://code.visualstudio.com/api/working-with-extensions/publishing-extension#platformspecific-extensions
