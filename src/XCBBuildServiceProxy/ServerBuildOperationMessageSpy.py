748af75fda172df28a2684059ecb35b8ceae283a
Ievgenii.Mykhalevskyi <Ievgenii.Mykhalevskyi@externals.adidas.com>
2026-02-10 21:55:11 +0200

    improved cancelation logic of session

--- src/XCBBuildServiceProxy/ServerBuildOperationMessageSpy.py
+++ src/XCBBuildServiceProxy/ServerBuildOperationMessageSpy.py
@@ -1,21 +1,34 @@
 import asyncio
+import time
 from MessageSpy import MessageSpyBase, MessageType
 from MessageReader import MessageReader, Message
 
 
 class ServerBuildOperationMessageSpy(MessageSpyBase):
     def __init__(self):
-        self.is_building = False
+        self._is_building = False
         self.sync_lock = asyncio.Lock()
+        self.mtime_of_build_operation_cancelled_message = None
+
+    @property
+    def is_building(self):
+        if self.mtime_of_build_operation_cancelled_message is not None:
+            # 20 seconds to cancel build operation, after that we assume that build operation is ended as we haven't received BUILD_OPERATION_ENDED message
+            if time.time() - self.mtime_of_build_operation_cancelled_message > 20:
+                self.mtime_of_build_operation_cancelled_message = None
+                self._is_building = False
+        return self._is_building
 
     async def on_receive_message(self, type: MessageType, message: MessageReader):
         async with self.sync_lock:
             message: Message = message.getMessage()
             if type == MessageType.server_message:
                 if message.message_code == b"BUILD_OPERATION_ENDED":
-                    self.is_building = False
+                    self._is_building = False
+                    self.mtime_of_build_operation_cancelled_message = None
             if type == MessageType.client_message:
                 if message.message_code == b"BUILD_START":
-                    self.is_building = True
-                # elif message.message_code == b"BUILD_CANCEL":
-                #     self.is_building = False
+                    self.mtime_of_build_operation_cancelled_message = None
+                    self._is_building = True
+                elif message.message_code == b"BUILD_CANCEL":
+                    self.mtime_of_build_operation_cancelled_message = time.time()
